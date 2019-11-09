4d012b386963439967d3aa677930c212db842e1a
Maxpa1n <775126459@qq.com>
2019-11-09 15:49:41 +0800

    add meat forward

--- models/MAML-wave/meta.py
+++ models/MAML-wave/meta.py
@@ -1,24 +1,85 @@
 import torch
 import torch.nn as nn
+from torch import optim
+from torch.nn import functional as F
+import numpy as np
+from learnmodel import Learner, Compute
+from SinwaveNShot import SinwaveNShot
 
 
 class Meta(nn.Module):
-    def __init__(self, hid_dim):
+    def __init__(self, hid_dim, meta_lr, update_lr):
         super(Meta, self).__init__()
-        self.input_layer = nn.Linear(1, hid_dim)
-        self.hid_layer = nn.Linear(hid_dim, hid_dim)
-        self.output_layer = nn.Linear(hid_dim, 1)
-        self.relu = nn.ReLU()
+        self.update_step = 5
+        self.inner_com = Compute(hid_dim)
+        self.outer_net = Learner(hid_dim)
+        self.meta_lr = meta_lr
+        self.update_lr = update_lr
+        self.outer_optim = optim.Adam(self.outer_net.parameters(), lr=self.update_lr)
 
-    def forward(self, X):
-        hid = self.relu(self.input_layer(X))
-        hid = self.relu(self.hid_layer(hid))
-        output = self.output_layer(hid)
-        return output
+    def forward(self, x_spt, y_spt, x_qry, y_qry):
+        task_num, setsz, _ = x_spt.size()
+        querysz = x_qry.size(1)
+
+        losses_q = [0 for _ in range(self.update_step + 1)]  # losses_q[i] is the loss on step i
+
+        for i in range(task_num):
+
+            # 1. run the i-th task and compute loss for k=0
+            logits = self.outer_net(x_spt[i], com=None)
+            loss = F.mse_loss(logits, y_spt[i])
+            grad = torch.autograd.grad(loss, self.outer_net.parameters(), allow_unused=True, retain_graph=True)
+            fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, self.outer_net.parameters())))
+
+            with torch.no_grad():
+                # [setsz, nway]
+                logits_q = self.outer_net(x_qry[i], com=None)
+                loss_q = F.mse_loss(logits_q, y_qry[i])
+
+                losses_q[0] += loss_q
+
+            # this is the loss and accuracy after the first update
+            with torch.no_grad():
+                # [setsz, nway]
+                logits_q = self.outer_net(x_qry[i], fast_weights)
+                loss_q = F.mse_loss(logits_q, y_qry[i])
+                losses_q[1] += loss_q
+
+            for k in range(1, self.update_step):
+                # 1. run the i-th task and compute loss for k=1~K-1
+                logits = self.outer_net(x_spt[i], fast_weights)
+
+                loss = F.mse_loss(logits, y_spt[i])
+                # 2. compute grad on theta_pi
+                grad = torch.autograd.grad(loss, fast_weights)
+                # 3. theta_pi = theta_pi - train_lr * grad
+                fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, fast_weights)))
+
+                logits_q = self.outer_net(x_qry[i], fast_weights)
+                # loss_q will be overwritten and just keep the loss_q on last update step.
+                loss_q = F.mse_loss(logits_q, y_qry[i])
+                losses_q[k + 1] += loss_q
+
+        # end of all tasks
+        # sum over all losses on query set across all tasks
+        loss_q = losses_q[-1] / task_num
+        # optimize theta parameters
+        self.outer_optim.zero_grad()
+        loss_q.backward()
+        # print('meta update')
+        # for p in self.net.parameters()[:5]:
+        # 	print(torch.norm(p).item())
+        self.outer_optim.step()
+        loss_avg = np.array(losses_q) / (querysz * task_num)
+        return loss_avg
 
 
 if __name__ == '__main__':
-    meta = Meta(64)
+    nshot = SinwaveNShot(2000, 20, 5, 5, 15, 'data')
+    x_spt, y_spt, x_qry, y_qry, param_spt, param_qry = nshot.next('train')
+    x_spt, y_spt, x_qry, y_qry = torch.from_numpy(x_spt), torch.from_numpy(y_spt), torch.from_numpy(
+        x_qry), torch.from_numpy(y_qry)
+    meta = Meta(hid_dim=64, meta_lr=1e-3, update_lr=1e-4)
     X = torch.randn(21, 1)
-    Y = meta(X)
-    print(Y.data)
+    Y = meta(x_spt, y_spt, x_qry, y_qry)
+    print('loss is {}'.format(Y))
